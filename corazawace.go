ef54f7b81f1fbedcf368fe79c63269638b969569
Fernando Outeda <fog_94@hotmail.com>
2022-03-15 17:22:13 -0300

    init

---
+++ corazawace.go
@@ -0,0 +1,35 @@
+package waceplugin
+
+import (
+	"fmt"
+
+	"github.com/corazawaf/coraza/v2"
+	"github.com/corazawaf/coraza/v2/operators"
+)
+
+type wace struct {
+}
+
+func (o *wace) Init(data string) error {
+	return nil
+}
+
+func (o *wace) Evaluate(tx *coraza.Transaction, value string) bool {
+	//res, capture := isSQLi(value)
+	a := tx.GetCollection(variables.FullRequest).Data()
+	for key, value := range a {
+		fmt.Print(key)
+		fmt.Print(":")
+		fmt.Println(value)
+	}
+	fmt.Println(a)
+	return false
+}
+
+func init() {
+	operators.RegisterPlugin("wace", func() coraza.RuleOperator { return new(wace) })
+}
+
+var (
+	_ coraza.RuleOperator = &wace{}
+)
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fog94/coraza-wace/v1
+
+go 1.16
+
+require github.com/corazawaf/coraza/v2 v2.0.0-rc.3
